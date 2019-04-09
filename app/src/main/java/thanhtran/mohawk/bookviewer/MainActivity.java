10fd0df73030897f972762dc3c28ab015354c3f5
thanhtrannn <thanh-tran@hotmail.com>
2019-04-08 20:17:52 -0400

    Initial commit

---
+++ app/src/main/java/thanhtran/mohawk/bookviewer/MainActivity.java
@@ -0,0 +1,14 @@
+// I, Thanh Tran, 000737993 certify that this material is my original work. No other person's work has been used without due acknowledgement.
+package thanhtran.mohawk.bookviewer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
